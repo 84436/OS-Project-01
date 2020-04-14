a102f25f6a35c68959b1ab51206cc33333fe0d32
Neo no Jams <alecmatthews0109@gmail.com>
2020-04-14 10:14:23 +0700

    Feature: history + echo cmd if called by "!! | !x""

--- src/main.c
+++ src/main.c
@@ -9,9 +9,63 @@
 #define LINE_LENGTH 100
 #define SHELLNAME "banhxeo> " //flexible <?>
 #define BUFFER_LENGTH 100
+#define HISTSIZE 10 //size of history array
 
+void init_history(char *history[LINE_LENGTH])
+{
+    for (int i = 0; i < HISTSIZE; i++)
+    {
+        history[i] = (char *)malloc(LINE_LENGTH);
+    }
+}
 
+void append_history(char *history[LINE_LENGTH], const char *cmd, int *history_count)
+{
+    int current_history = (int)*history_count;
 
+    if (current_history == 0)
+    {
+        strcpy(history[0], cmd);
+        current_history = current_history + 1;
+    }
+    else if (current_history < HISTSIZE)
+    {
+        strcpy(history[current_history], cmd);
+        current_history = current_history + 1;
+    }
+    else
+    {
+        for (int i = 1; i < HISTSIZE; i++)
+        {
+            strcpy(history[i - 1], history[i]);
+        }
+
+        strcpy(history[HISTSIZE - 1], cmd);
+    }
+
+    *history_count = current_history;
+}
+
+char *get_history(char *history[LINE_LENGTH], int history_count, const char x)
+{
+    int index = x - '0';
+
+    if (index > history_count || index - 1 < 0 || history_count == 0)
+    {
+        return NULL;
+    }
+    return history[index - 1];
+}
+
+void export_history(char *history[LINE_LENGTH], int history_count)
+{
+    if (history[0] == NULL)
+        printf("No history found");
+    for (int i = 0; i < history_count; i++)
+    {
+        printf("[%d] %s\n", i + 1, history[i]);
+    }
+}
 
 void child(char *argv[])
 {
@@ -85,6 +139,11 @@ int main() {
     int status = 0;
     char user_cmd[LINE_LENGTH];
 
+    //where the history is saved
+    int history_count = 0;
+    char *cmd_history[LINE_LENGTH];
+    init_history(cmd_history);
+
     while (is_running) {
         printf(SHELLNAME);
         fflush(stdout);
@@ -98,6 +157,33 @@ int main() {
 
         if (strcmp(user_cmd, "exit") == 0) {
             is_running = false;
+            continue;
+        }
+
+        if (user_cmd[0] == '!' && (user_cmd[1] == '!' || user_cmd[1] != '\0'))
+        {
+            if (user_cmd[1] == '!')
+                user_cmd[1] = '1';
+
+            char *last_cmd = get_history(cmd_history, history_count, user_cmd[1]);
+            if (last_cmd != NULL){
+                strcpy(user_cmd, last_cmd);
+                printf("%s%s\n",SHELLNAME, user_cmd);
+                fflush(stdout);
+            }
+            else {
+                printf("No command available\n");
+                fflush(stdout);
+                continue;
+            }
+        }
+
+        // append new cmd to history
+        append_history(cmd_history, user_cmd, &history_count);
+
+        if (strcmp(user_cmd, "history") == 0)
+        {
+            export_history(cmd_history, history_count);
             continue;
         }
 
@@ -124,6 +210,8 @@ int main() {
         }
 
     }
+
+    //Free history space?
 
     return 0;
 }
